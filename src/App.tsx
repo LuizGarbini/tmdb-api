8d32f1ecb109f3d5ae7499b06d3b7b37dcf8f6f4
LuizGarbini <garbasneto@gmail.com>
2025-04-18 14:47:01 -0300

    listagem dos filmes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+	const [movies, setMovies] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const getMovies = () => {
+		axios({
+			method: "get",
+			url: "https://api.themoviedb.org/3/discover/movie",
+			params: {
+				api_key: "5428078da291e842d9062de26fc1c031",
+				language: "pt-BR",
+			},
+		}).then((response) => {
+			console.log(response);
+		});
+	};
+
+	getMovies();
+
+	return (
+		<div>
+			<h1>Hello World</h1>
+		</div>
+	);
 }
-
-export default App
